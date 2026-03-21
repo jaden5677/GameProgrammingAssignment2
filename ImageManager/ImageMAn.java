34af7e3256df8f1fe907f2a7a5844f41ac55b239
Jaden Sooklal <jadensooklal5677@gmail.com>
2026-03-20 21:51:27 -0400

    Add ImageMAn class and Main class with main method

---
+++ ImageManager/ImageMAn.java
@@ -0,0 +1,5 @@
+package ImageManager;
+
+public abstract class ImageMAn{
+    private ImageMAn(){}
+}
---
+++ MainClass/Main.java
@@ -0,0 +1,6 @@
+package MainClass;
+public class Main{
+    public static void main(){
+        System.out.println("Hello World");
+    }
+}
